3e4ce6d2a89160ba34bcbf7b182b15e2229dda38
Manish Maheshwari <manish.maheshwari@ag-technologies.com>
2022-02-19 11:15:34 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StockService } from './stock.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'trinkerr_test';
+
+  token: string = '';
+
+  constructor(private stockService: StockService) {
+    
+  }
+
+  ngOnInit(): void {
+    this.getUserToken();
+  }
+
+  getUserToken() {
+    this.stockService.getToken().subscribe(
+      result => {
+        console.log(result);
+        this.token = result.token;
+      },
+      err => {
+        console.log(err);
+      },
+      () => {
+        this.fetchStock(this.token);
+      }
+    )
+  }
+
+  fetchStock(token: string) {
+    this.stockService.getStock(token).subscribe(
+      stock => {
+        console.log(stock);
+      }
+    )
+  }
 }
---
+++ src/app/stock.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StockService {
+
+  BASE_URL = 'http://3.109.141.224:5000';
+
+  constructor(private http: HttpClient) { }
+
+  getToken(): Observable<any> {
+    return this.http.get(`${this.BASE_URL}/api/user-access-token`);
+  }
+
+  getStock(token: string): Observable<any> {
+    let header = new Headers();
+    header.set('user-access-token', token)
+    return this.http.get(`${this.BASE_URL}/api/data?search_string=`, {'headers':header})
+  }
+
+}
